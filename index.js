b32f607cf57f554697e1da071bed0c15fe959803
shia <shia@amsg.tk>
2020-03-29 22:39:18 +0800

    digest, md5, sha256, sha512

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const crypto = require('crypto');
+
 module.exports = {
 	delay,
 	promiseAny,
+	digest,
+	md5,
+	sha256,
+	sha512,
 };
 
 
@@ -16,3 +22,23 @@ function _reversePromise(promise) {
 function promiseAny(iterable) {
 	return _reversePromise(Promise.all([...iterable].map(_reversePromise)));
 }
+
+function digest(algorithm, data, hmackey) {
+	const cr = hmackey? crypto.createHmac(algorithm, hmackey):
+		crypto.createHash(algorithm);
+	cr.update(data, 'utf8');
+	return cr.digest('hex');
+}
+
+function md5(data) {
+	return digest('md5', data);
+}
+
+function sha256(data, hmackey) {
+	return digest('sha256', data, hmackey);
+}
+
+function sha512(data, hmackey) {
+	return digest('sha512', data, hmackey);
+}
+
