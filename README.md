9b8979ac96dfd20a2c96a4e55498220633227c58
shia <shia@amsg.tk>
2020-03-30 02:04:01 +0800

    QPush & QGroup class

--- README.md
+++ README.md
@@ -41,3 +41,21 @@ for (const f of traverseDir('FOLDER')) console.log(f);
 Async call `worker(list[idx], idx, list, label)` every item of `list` parallelly.
 `size` is the thread pool size;
 `showError` can be true, false, or an async function.
+
+
+-----
+
+## QPush & QGroup class
+
+Send push notification to QPush app for iOS.
+
+```js
+const {QPush, QGroup} = require('shiajs/qpush');
+
+const push = new QPush('name', 'code');
+push.log('push some log'); // push.info() is the same
+push.error(new Error('push an error'));
+
+const group = new QGroup(['name1', 'code1'], ['name2', 'code2']);
+group.info('info to group').error('push error to group');
+```
---
+++ qpush.js
@@ -0,0 +1,83 @@
+const rp = require('request-promise');
+const {$log, timeiso} = require('.');
+
+class QPush {
+	constructor(name, code) {
+		if (Array.isArray(name)) {
+			[name, code] = name;
+		}
+		if (typeof name!=='string' || typeof code!=='string') {
+			throw new Error('QPush needs name and code to init');
+		}
+		this.auth = {name, code};
+	}
+
+	async send(data) {
+		try {
+			const body = await rp({
+				json: true,
+				method: 'POST',
+				url: 'https://qpush.me/pusher/push_site/',
+				form: {
+					...this.auth,
+					sig: '',
+					//cache: false,
+					...data,
+				},
+			});
+			if (body && body.error) throw new Error(body.error);
+			return body;
+		}
+		catch(e) {
+			$log('[QPush Error]', e.message);
+		}
+	}
+
+	async text(text) {
+		return this.send({
+			'msg[text]': text.slice(0,500),
+		});
+	}
+
+	async url(title, url) {
+		return this.send({
+			'msg[text]': url,
+			'msg[type]': 'url',
+			'msg[extra][title]': title,
+		});
+	}
+
+	async log(message) {
+		return this.text(`[info] ${timeiso().slice(5)}\n${message}`);
+	}
+
+	async info(message) {
+		return this.log(message);
+	}
+
+	async error(error) {
+		const message = (error instanceof Error)? error.message: error;
+		return this.text(`[ERROR] ${timeiso().slice(5)}\n${message}`);
+	}
+}
+
+class QGroup {
+	constructor(...users) {
+		if (!Array.isArray(users[0])) {
+			throw new Error('QGroup needs name code pairs to init')
+		}
+		if (Array.isArray(users[0][0])) [users]=users;
+		this.instances = users.map(pair => new QPush(pair));
+		'text,url,info,log,error'.split(',').map(fn => {
+			this[fn] = (...args) => {
+				this.instances.forEach(push => push[fn](...args));
+				return this;
+			};
+		});
+	}
+}
+
+module.exports = {
+	QPush,
+	QGroup,
+};
