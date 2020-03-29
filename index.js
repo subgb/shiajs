a5234e119db4027c05c2bc6c696663dec7d18a06
shia <shia@amsg.tk>
2020-03-29 22:30:03 +0800

    promiseAny

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 module.exports = {
 	delay,
+	promiseAny,
 };
 
 
 function delay(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function _reversePromise(promise) {
+	return new Promise((r, j) => Promise.resolve(promise).then(j, r));
+}
+
+// resolve while any promise fulfills, or reject while all promises rejected.
+function promiseAny(iterable) {
+	return _reversePromise(Promise.all([...iterable].map(_reversePromise)));
 }
